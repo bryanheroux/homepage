ed022e4f80c2b2deab10d4b8410df4dd786f0761
Timm Stokke <timm@stokke.me>
2017-03-01 17:25:13 -0800

    Small update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -34,12 +34,13 @@ import gulpLoadPlugins from 'gulp-load-plugins';
 import {output as pagespeed} from 'psi';
 import pkg from './package.json';
 
+const htmlPartial = require('gulp-html-partial');
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js','!node_modules/**'])
+  gulp.src(['app/scripts/**/*.js', '!node_modules/**'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failAfterError()))
@@ -129,7 +130,7 @@ gulp.task('scripts', () =>
 
 // Scan your HTML for assets & optimize them
 gulp.task('html', () => {
-  return gulp.src('app/**/*.html')
+  return gulp.src(['app/**/*.html', '!app/partials/*.html'])
     .pipe($.useref({
       searchPath: '{.tmp,app}',
       noAssets: true
@@ -149,14 +150,31 @@ gulp.task('html', () => {
     })))
     // Output files
     .pipe($.if('*.html', $.size({title: 'html', showFiles: true})))
+
+    .pipe(htmlPartial({
+      basePath: 'app/partials/'
+    }))
+
     .pipe(gulp.dest('docs'));
+});
+
+
+// Scan your HTML for assets & optimize them
+gulp.task('devHtml', () => {
+  console.log('yey!');
+  return gulp.src('app/**/*.html')
+    // partials
+    .pipe(htmlPartial({
+      basePath: 'app/partials/'
+    }))
+    .pipe(gulp.dest('.tmp'));
 });
 
 // Clean output directory
 gulp.task('clean', () => del(['.tmp', 'docs/*', '!docs/.git', '!docs/CNAME'], {dot: true}));
 
 // Watch files for changes & reload
-gulp.task('serve', ['scripts', 'styles'], () => {
+gulp.task('serve', ['scripts', 'styles', 'devHtml'], () => {
   browserSync({
     notify: false,
     // Customize the Browsersync console logging prefix
@@ -171,7 +189,7 @@ gulp.task('serve', ['scripts', 'styles'], () => {
     port: 3000
   });
 
-  gulp.watch(['app/**/*.html'], reload);
+  gulp.watch(['app/**/*.html'], ['devHtml', reload]);
   gulp.watch(['app/styles/**/*.{scss,css}'], ['styles', reload]);
   gulp.watch(['app/scripts/**/*.js'], ['lint', 'scripts', reload]);
   gulp.watch(['app/images/**/*'], reload);
@@ -206,7 +224,7 @@ gulp.task('default', ['clean'], cb =>
 // Run PageSpeed Insights
 gulp.task('pagespeed', cb =>
   // Update the below URL to the public URL of your site
-  pagespeed('example.com', {
+  pagespeed('herouxhomes.ca', {
     strategy: 'mobile'
     // By default we use the PageSpeed Insights free (no API key) tier.
     // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
